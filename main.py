da0bb03fc8cf7154b1058c87b480d98b53df5367
username <you@example.com>
2024-03-28 20:40:19 -0600

    Created word_count function, made main function lowercase

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def Main():
+def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(file_contents)
+        word_count(file_contents)
 
-Main()
+#count words function
+def word_count(file_contents):
+    words = file_contents.split()
+    count = 0
+    for word in words:
+        count += 1
+    #print(count)
+    return count
+
+main()
